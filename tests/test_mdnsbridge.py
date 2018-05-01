f87a8dae9fbfc95ff8b6c77e82e1c9624ed520d2
Simon Rankine <simon.rankine@bbc.co.uk>
2018-05-01 14:32:58 +0100

    Put some tests back in

--- tests/test_mdnsbridge.py
+++ tests/test_mdnsbridge.py
@@ -14,8 +14,10 @@
 
 import unittest
 import mock
+import json
 
 from mdnsbridge.mdnsbridge import *
+
 
 class StubWebAPI(object):
     pass
@@ -25,6 +27,82 @@ class AbortException(Exception):
     def __init__(self, code, *args, **kwargs):
         self.code = code
         super(AbortException, self).__init__(*args, **kwargs)
+
+
+class TestmDNSBridgeAPI(unittest.TestCase):
+
+    def setUp(self):
+        self.APIBASE = "/{}/{}/{}/".format(APINAMESPACE, APINAME, APIVERSION)
+        self.mock_mdns_get_services()
+        # Expse Werkzeug test client to test API
+        flaskr = mDNSBridgeAPI(self.mdns)
+        flaskr.app.config['TESTING'] = True
+        client = flaskr.app.test_client()
+        self.client = client
+
+    def mock_mdns_get_services(self):
+        self.mdns = mock.MagicMock()
+        # the mock mdns needs to mock the get_services method
+        # make it reflect what it is passed
+
+        def behaviour(passedValue):
+            return passedValue
+        self.mdns.get_services = behaviour
+
+    def inspect_endpoint(self, path, expected, resourceName):
+        # Get reponse from test client, compare to expected
+        rv = self.client.get(path)
+        actual = json.loads(rv.data)
+        message = ("{} resource at '/' should return "
+                   "{}, got {}").format(resourceName, expected, actual)
+        self.assertEqual(actual, expected, msg=message)
+
+    def test_namespace_resource(self):
+        self.inspect_endpoint(
+            path='/',
+            expected=[APINAMESPACE + '/'],
+            resourceName="Namespace"
+        )
+
+    def test_apinamespace_resource(self):
+        self.inspect_endpoint(
+            path='/{}/'.format(APINAMESPACE),
+            expected=[APINAME + '/'],
+            resourceName="API Namespace"
+        )
+
+    def test_version_resource(self):
+        myPath = '/{}/{}/'.format(APINAMESPACE,APINAME)
+        self.inspect_endpoint(
+            path=myPath,
+            expected=[APIVERSION + '/'],
+            resourceName="Version"
+        )
+
+    def test_base_resource(self):
+        myExpected = {
+            "resources": [
+                "nmos-query/",
+                "nmos-registration/"
+            ]
+        }
+        self.inspect_endpoint(
+            path=self.APIBASE,
+            expected=myExpected,
+            resourceName="Base"
+        )
+
+    def test_type_resource(self):
+        myExpected = {
+            "representation": "nmos-query"
+        }
+        myPath = self.APIBASE + "nmos-query/"
+        self.inspect_endpoint(
+            path=myPath,
+            expected=myExpected,
+            resourceName="Base"
+        )
+
 
 class TestmDNSBridge(unittest.TestCase):
     @mock.patch('mdnsbridge.mdnsbridge.MDNSEngine')
