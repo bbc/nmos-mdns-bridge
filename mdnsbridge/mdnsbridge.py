e3c12cfe19cda7d7543eec1dc25e7926d59d1d47
dannymeloy <37411379+dannymeloy@users.noreply.github.com>
2018-12-13 15:12:01 +0000

    Added nmos-security mdns type

--- mdnsbridge/mdnsbridge.py
+++ mdnsbridge/mdnsbridge.py
@@ -22,7 +22,7 @@ from nmoscommon.mdns import MDNSEngine
 from flask import abort
 from nmoscommon import nmoscommonconfig
 
-VALID_TYPES = ["nmos-query", "nmos-registration"]
+VALID_TYPES = ["nmos-query", "nmos-registration", "nmos-security"]
 
 APINAMESPACE = "x-ipstudio"
 APINAME = "mdnsbridge"
